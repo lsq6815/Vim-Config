3951f32a14c10fc8bea2515eeea0228b6bd9c8d7
lsq6815 <lsq6815@gmail.com>
2021-01-26 03:54:43 -0500

    ADD: templates dir

---
+++ templates/skeleton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>  /* Supply integer type with exact bits */
+#include <stddef.h>  /* Supply some definition like ptrdiff_t, size_t etc. */
+#include <stdbool.h> /* Supply type _Bool, false and true */
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
